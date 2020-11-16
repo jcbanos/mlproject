4f695ea5de229b6124214ded0339221a398ab97d
Juan Carlos Banos Mafud <juancarlosbanosmafud@gmail.com>
2020-11-16 10:19:56 -0600

    Finised the test

--- tests/tool_test.py
+++ tests/tool_test.py
@@ -1,6 +1,6 @@
 from mlproject.tools import haversine
 
 def test_haversine():
-  assert haversine(48.865070, 2.380009, 19.3584709, -99.1827227) == 11159.056396758873
+  assert haversine(2.380009, 48.865070, -99.1827227, 19.3584709) == 9207.446970959321
 
 
